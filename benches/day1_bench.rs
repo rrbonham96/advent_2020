9eadbe3b3db949083ef9ce7d4b2499516e06a1c8
Reilly Bonham <rrbonham96@gmail.com>
2020-12-02 12:29:52 -0600

    Fix day 1 bench

--- benches/day1_bench.rs
+++ benches/day1_bench.rs
@@ -2,18 +2,18 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use advent_2020::day1::*;
 
 pub fn complement_benchmark(c: &mut Criterion) {
+    let list = list_of_nums("inputs/day1_input.txt");
     c.bench_function("Day 1 - Part 1",
      |b| b.iter(|| {
-            let list = list_of_nums("inputs/day1_input.txt");
             complements(black_box(&list), black_box(2020));
         })
     );
 }
 
 pub fn three_complement_benchmark(c: &mut Criterion) {
+    let list = list_of_nums("inputs/day1_input.txt");
     c.bench_function("Day 1 - Part 2",
      |b| b.iter(|| {
-            let list = list_of_nums("inputs/day1_input.txt");
             three_complements(black_box(&list), black_box(2020));
         })
     );
